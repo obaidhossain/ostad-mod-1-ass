d8aa52d53065f8a0f606fb51678c9a07e6f5b149
Md. Obaid Hossain <obaidhossain.me@gmail.com>
2023-09-13 15:34:27 +0600

    Update even_odd_checker.php

--- even_odd_checker.php
+++ even_odd_checker.php
@@ -1,6 +1,8 @@
 <?php
 /*
  Build a PHP program called even_odd_checker.php that checks whether a given number is even or odd. Provide an input field where the user can enter a number. Display a message indicating whether the number is even or odd.
+
+ Preview Link: https://themenut.com/sample/ostad/even-odd-checker.php
  */
 ?>
 <!DOCTYPE html>
